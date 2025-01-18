0c958d6a64102b7c39a7e600257904ae9e846b55
marfralarf <139411852+marfralarf@users.noreply.github.com>
2025-01-17 21:21:25 -0600

    Update Character.java

--- Character.java
+++ Character.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 public class Character{
   int[] pos = {0, 0};
   public Character(){
@@ -6,5 +7,18 @@ public class Character{
   }
   public Character(int[] initPos){
     pos = initPos;
+  }
+  public int[] getPos(Character guy){
+    return this.pos;
+  }
+  
+  ArrayList<Character> holes = new ArrayList<Character>();
+
+  public ArrayList<Character> digHole(){
+    int[] holePos = this.getPos(this);
+    Character hole = new Character(holePos);
+    holes.add(hole);
+    System.out.print(holes);
+    return holes;
   }
 }
