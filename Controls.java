b1795f3c16eebc64bb4a5dbad44173bef655f171
marfralarf <139411852+marfralarf@users.noreply.github.com>
2025-01-17 21:22:19 -0600

    Update Controls.java

--- Controls.java
+++ Controls.java
@@ -1,14 +1,14 @@
 import java.util.Scanner;
 public class Controls{
-  public static char moveInput(){
-    Scanner directions = new Scanner(System.in);
-    System.out.println("Pick your direction of movement");
-    String input = directions.nextLine();
-    return input.charAt(0);
-  }
-  public static int[] charVector(char nesw){
-    int[] move = {0,0};
-    switch (nesw){
+    public static char moveInput(){
+        Scanner directions = new Scanner(System.in);
+        System.out.println("Pick your direction of movement");
+        String input = directions.nextLine();
+        return input.charAt(0);
+    }
+    public static int[] charVector(Character hero, char nesw){
+        int[] move = {0,0};
+        switch (nesw){
         case 'n':
             move[1] = -1;
         break;
@@ -20,6 +20,9 @@ public class Controls{
         break;
         case 'w':
             move[0] = -1;
+        break;
+        case 'x':
+            hero.digHole();
         break;
     }
     return move;
