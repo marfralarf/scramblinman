1ba55bb3bd94dbc9272878a6ec84c01a68160c8a
marfralarf <139411852+marfralarf@users.noreply.github.com>
2025-02-08 14:29:13 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,26 +1,30 @@
+import java.util.*;
 public class Main {
-  static int[] roomSize = {10, 10};
-  
-  public static void main(String[] args){
-      char[][] points = Room.buildMap(roomSize[0], roomSize[1]);
-      Room mainMap = new Room(points);
-      Character hero = new Character();
+  static int[] roomSize = { 10, 10 };
 
-      mainMap.spawnChar(hero.pos[0], hero.pos[1]);
-      System.out.println(" ");
-      mainMap.render(roomSize[0], roomSize[1]);
-    while (hero.pos[0] < 20){
-      char move = Controls.moveInput();
-      int[] movement = Controls.charVector(hero, move);
-      moveChar(mainMap, hero, movement);
-      }
-  }
-    public static void moveChar(Room mainMap, Character hero, int[] movement){
-      hero.pos[0] += movement[1];
-      hero.pos[1] += movement[0];
-      
-      mainMap.loopMap(roomSize, hero);
-      mainMap.spawnChar(hero.pos[0], hero.pos[1]);
-      mainMap.render(roomSize[0], roomSize[1]);
+  public static void main(String[] args) {
+    Game newGame = new Game(roomSize[0], roomSize[1]);
+    char[][] points = Room.buildMap(roomSize[0], roomSize[1]);
+    Room mainMap = new Room(points);
+    Character hero = new Character();
+    ArrayList<Entity> guys = new ArrayList<Entity>();
+    guys.add(hero);
+    guys.add(newGame);
+    Controls user = new Controls();
+
+    mainMap.spawnChar(guys);
+    System.out.println(" ");
+    mainMap.render(roomSize[0], roomSize[1]);
+    
+    while (hero.pos[0] < 20) {
+      char move = user.moveInput();
+      int[] movement = user.charVector(guys, move);
+      hero.moveChar(mainMap, guys, movement);
     }
   }
+  public static int addScore(int attempts, int score){
+    score += attempts;
+    return score;
+  }
+
+}
