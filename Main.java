00fc5f5eb7be51e54541265ff364a889959cc642
marfralarf <139411852+marfralarf@users.noreply.github.com>
2025-02-09 21:42:23 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,25 +1,42 @@
 import java.util.*;
+import javax.swing.*;
+
 public class Main {
   static int[] roomSize = { 10, 10 };
 
   public static void main(String[] args) {
-    Game newGame = new Game(roomSize[0], roomSize[1]);
+    JFrame frame = new JFrame("My app");
+    frame.setSize(300, 300);
+    GUI newGUI = new GUI();
+
+    ArrayList<Entity> guys = gameStart();
+    Character hero = (Character)guys.get(0);
+
     char[][] points = Room.buildMap(roomSize[0], roomSize[1]);
     Room mainMap = new Room(points);
-    Character hero = new Character();
-    ArrayList<Entity> guys = new ArrayList<Entity>();
-    guys.add(hero);
-    guys.add(newGame);
     Controls user = new Controls();
 
     mainMap.spawnChar(guys);
+    //newGUI.makeButtons(frame);
+    //newGUI.fillPanels(frame, mainMap);
     System.out.println(" ");
     mainMap.render(roomSize[0], roomSize[1]);
-    
+    frame.setVisible(true);
+
     while (hero.pos[0] < 20) {
-      char move = user.moveInput();
+      String[] move = user.moveInput();
       int[] movement = user.charVector(guys, move);
       hero.moveChar(mainMap, guys, movement);
     }
+  }
+  public static ArrayList<Entity> gameStart(){
+    Game newGame = new Game(roomSize[0], roomSize[1]);
+    Character hero = new Character();
+    ArrayList<Entity> guys = new ArrayList<Entity>();
+    newGame.findWrapDistance(newGame.pos, hero.pos);
+    guys.add(hero);
+    guys.add(newGame);
+
+    return guys;
   }
 }
