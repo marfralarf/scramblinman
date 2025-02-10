56b2442a2822800999ba1c31beba65f48a3edc52
marfralarf <139411852+marfralarf@users.noreply.github.com>
2025-02-09 21:41:23 -0600

    Update Controls.java

--- Controls.java
+++ Controls.java
@@ -4,38 +4,39 @@ public class Controls {
     public Controls() {
     }
 
-    public char moveInput() {
+    public String[] moveInput() {
         Scanner directions = new Scanner(System.in);
         System.out.println("Pick your direction of movement. Press n, e, s, or w to move and x to dig a hole!");
         String input = directions.nextLine();
-        return input.charAt(0);
+        return input.split("");
     }
 
-    public int[] charVector(ArrayList<Entity> guys, char news) {
+    public int[] charVector(ArrayList<Entity> guys, String[] news) {
         int[] move = { 0, 0 };
         Character hero = (Character)guys.get(0);
-        switch (news) {
-            case 'n':
-                move[1] = -1;
+        for (String letter : news){
+        switch (letter) {
+            case "n":
+                move[1] += -1;
                 break;
-            case 'e':
-                move[0] = 1;
+            case "e":
+                move[0] += 1;
                 break;
-            case 's':
-                move[1] = 1;
+            case "s":
+                move[1] += 1;
                 break;
-            case 'w':
-                move[0] = -1;
+            case "w":
+                move[0] += -1;
                 break;
-            case 'x':
+            case "x":
                 guys.add(hero.digHole());
                 Game currentGame = (Game)guys.get(1);
                 if (currentGame.checkTreasure(guys))
-                    currentGame.gameWin(guys);
+                    guys = currentGame.gameWin(guys);
                 break;
             default:
                 break;
-        }
+        }}
         return move;
     }
 }
